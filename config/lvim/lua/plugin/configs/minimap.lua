18af7bf157c4deea21b63f847832d2c2f4a88cdc
batman <batman@brucewayne>
2021-09-09 10:04:38 +0700

    update

---
+++ config/lvim/lua/plugin/configs/minimap.lua
@@ -0,0 +1,17 @@
+local M = function()
+	vim.g.minimap_width = 10
+	vim.g.minimap_auto_start = 1
+	vim.g.minimap_auto_start_win_enter = 1
+	vim.g.minimap_close_filetypes = {
+		"Outline",
+		"dashboard",
+		"help",
+		"markdown",
+		"packer",
+		"rnvimr",
+		"starify",
+		"toggleterm",
+		"vimwiki",
+	}
+end
+return M
